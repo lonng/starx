61d5dee0d59d3f609d037e43ca0839d6a40c6697
Chris Lonng <chris@lonng.org>
2015-12-02 14:00:41 +0800

    精简app

--- app.go
+++ app.go
@@ -8,33 +8,23 @@ import (
 type _app struct {
 	Master     *ServerConfig      // master server config
 	Config     *ServerConfig      // current server info
-	removeChan chan string        // remove server channel
-	registChan chan *ServerConfig // add server channel
 }
 
 func newApp() *_app {
-	return &_app{
-		removeChan: make(chan string, 10),
-		registChan: make(chan *ServerConfig, 10)}
+	return &_app{}
 }
 
 func (app *_app) start() {
 	var endRunning = make(chan bool, 1)
 	app.loadDefaultComps()
-
-	// enable port listener
-	go app.listenPort()
+	// enable heartbeat service
 	go heartbeatService.start()
-	// main goroutine
-	app.listenChan()
-
+	// enable port listener
+	app.listenPort()
 	<-endRunning
 	Info("server: " + app.Config.Id + " is stopping...")
 	// close all channels
-	close(app.registChan)
-	close(app.removeChan)
 	close(endRunning)
-
 	// close all of components
 	remote.close()
 }
@@ -60,17 +50,6 @@ func (app *_app) listenPort() {
 			go handler.handle(conn)
 		} else {
 			go remote.handle(conn)
-		}
-	}
-}
-
-func (app *_app) listenChan() {
-	for {
-		select {
-		case svr := <-app.registChan:
-			registerServer(*svr)
-		case svrId := <-app.removeChan:
-			removeServer(svrId)
 		}
 	}
 }
