1af12f474c6266b5a25b63c7d7cb1d3913737c51
Chris Lonng <chris@lonng.org>
2015-12-16 21:33:49 +0800

    拆分session文件

--- session.go
+++ session.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"net"
 	"time"
 	"starx/rpc"
 )
@@ -38,59 +37,12 @@ type Session struct {
 	rawSessionId uint64        // raw session id, frontendSession in frontend server, or backendSession in backend server
 }
 
-// Session for frontend server, used for store raw socket information
-// only used in package internal, can not accessible by other package
-type handlerSession struct {
-	id          uint64
-	socket      net.Conn
-	status      SessionStatus
-	userSession *Session
-	lastTime    int64 // last heartbeat unix time stamp
-}
-
-// Session for backend server, used for store raw socket information
-// only used in package internal, can not accessible by other package
-type remoteSession struct {
-	id            uint64
-	socket        net.Conn
-	status        SessionStatus
-	sessionMap    map[uint64]*Session
-	fsessionIdMap map[uint64]uint64 // session id map(frontend session id -> backend session id)
-	bsessionIdMap map[uint64]uint64 // session id map(backend session id -> frontend session id)
-	lastTime      int64 // last heartbeat unix time stamp
-}
-
 // Create new session instance
 func newSession() *Session {
 	return &Session{
 		Id:       connectionService.getNewSessionUUID(),
 		status:   SS_START,
 		lastTime: time.Now().Unix()}
-}
-
-// Create new frontend session instance
-func newHandlerSession(id uint64, conn net.Conn) *handlerSession {
-	hs := &handlerSession{
-		id:       id,
-		socket:   conn,
-		status:   SS_START,
-		lastTime: time.Now().Unix()}
-	session := newSession()
-	session.rawSessionId = hs.id
-	hs.userSession = session
-	return hs
-}
-
-// Create new backend session instance
-func newRemoteSession(id uint64, conn net.Conn) *remoteSession {
-	return &remoteSession{
-		id:            id,
-		socket:        conn,
-		status:        SS_START,
-		sessionMap:    make(map[uint64]*Session),
-		fsessionIdMap: make(map[uint64]uint64),
-		bsessionIdMap: make(map[uint64]uint64),
-		lastTime:      time.Now().Unix()}
 }
 
 // Session send packet data
@@ -142,51 +94,6 @@ func (session *Session) Response(data []byte) {
 			resp.Sid = sid
 			writeResponse(rs, &resp)
 		}
-	}
-}
-
-// Implement Stringer interface
-func (hs *handlerSession) String() string {
-	return fmt.Sprintf("id: %d, remote address: %s, last time: %d",
-		hs.id,
-		hs.socket.RemoteAddr().String(),
-		hs.lastTime)
-}
-
-func (hs *handlerSession) send(data []byte) {
-	hs.socket.Write(data)
-}
-
-func (hs *handlerSession) heartbeat() {
-	hs.lastTime = time.Now().Unix()
-}
-
-// Implement Stringer interface
-func (rs *remoteSession) String() string {
-	return fmt.Sprintf("id: %d, remote address: %s, last time: %d",
-		rs.id,
-		rs.socket.RemoteAddr().String(),
-		rs.lastTime)
-}
-
-func (rs *remoteSession) send(data []byte) {
-	rs.socket.Write(data)
-}
-
-func (rs *remoteSession) heartbeat() {
-	rs.lastTime = time.Now().Unix()
-}
-
-func (rs *remoteSession) GetUserSession(sid uint64) *Session {
-	if bsid, ok := rs.fsessionIdMap[sid]; ok && bsid > 0 {
-		return rs.sessionMap[bsid]
-	} else {
-		session := newSession()
-		session.rawSessionId = rs.id
-		rs.fsessionIdMap[sid] = session.Id
-		rs.sessionMap[session.Id] = session
-		rs.bsessionIdMap[session.Id] = sid
-		return session
 	}
 }
 
