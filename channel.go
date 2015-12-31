fc796bafac704e92280b00e7b1415659ea71b101
Chris Lonng <chris@lonng.org>
2015-12-31 16:52:32 +0800

    refactor: separator channelService from channel.go

--- channel.go
+++ channel.go
@@ -62,38 +62,3 @@ func (c *Channel) GetCount() int {
 func (c *Channel) Destroy() {
 	delete(c.channelServive.channels, c.name)
 }
-
-type ChannelServive struct {
-	channels map[string]*Channel // all server channels
-}
-
-func newChannelServive() *ChannelServive {
-	return &ChannelServive{}
-}
-
-func (c *ChannelServive) NewChannel(name string) *Channel {
-	channel := NewChannel(name, c)
-	c.channels[name] = channel
-	return channel
-}
-
-// Get channel by channel name
-func (c *ChannelServive) GetChannel(name string) (*Channel, bool) {
-	channel, exists := c.channels[name]
-	return channel, exists
-}
-
-// Get all members in channel by channel name
-func (c *ChannelServive) GetMembers(name string) []int {
-	if channel, ok := c.channels[name]; ok {
-		return channel.GetMembers()
-	}
-	return make([]int, 0)
-}
-
-// Destroy channel by channel name
-func (c *ChannelServive) DestroyChannel(name string) {
-	if channel, ok := c.channels[name]; ok {
-		channel.Destroy()
-	}
-}
