b4db1a09b69dde93496d8ca20edc7d53dc1c2f88
chrislonng <chris@lonng.org>
2017-01-13 17:11:47 +0800

    refactor: change session API, add unit test

--- session/session.go
+++ session/session.go
@@ -116,180 +116,180 @@ func (s *Session) Set(key string, value interface{}) {
 	s.data[key] = value
 }
 
-func (s *Session) Int(key string) (int, error) {
+func (s *Session) HasKey(key string) bool {
+	_, has := s.data[key]
+	return has
+}
+
+func (s *Session) Int(key string) int {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(int)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Int8(key string) (int8, error) {
+func (s *Session) Int8(key string) int8 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(int8)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Int16(key string) (int16, error) {
+func (s *Session) Int16(key string) int16 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(int16)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Int32(key string) (int32, error) {
+func (s *Session) Int32(key string) int32 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(int32)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Int64(key string) (int64, error) {
+func (s *Session) Int64(key string) int64 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(int64)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Uint(key string) (uint, error) {
+func (s *Session) Uint(key string) uint {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(uint)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Uint8(key string) (uint8, error) {
+func (s *Session) Uint8(key string) uint8 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(uint8)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Uint16(key string) (uint16, error) {
+func (s *Session) Uint16(key string) uint16 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(uint16)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Uint32(key string) (uint32, error) {
+func (s *Session) Uint32(key string) uint32 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(uint32)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Uint64(key string) (uint64, error) {
+func (s *Session) Uint64(key string) uint64 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(uint64)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Float32(key string) (float32, error) {
+func (s *Session) Float32(key string) float32 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(float32)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Float64(key string) (float64, error) {
+func (s *Session) Float64(key string) float64 {
 	v, ok := s.data[key]
 	if !ok {
-		return 0, ErrKeyNotFound
+		return 0
 	}
 
 	value, ok := v.(float64)
 	if !ok {
-		return 0, ErrWrongValueType
+		return 0
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) String(key string) (string, error) {
+func (s *Session) String(key string) string {
 	v, ok := s.data[key]
 	if !ok {
-		return "", ErrKeyNotFound
+		return ""
 	}
 
 	value, ok := v.(string)
 	if !ok {
-		return "", ErrWrongValueType
+		return ""
 	}
-	return value, nil
+	return value
 }
 
-func (s *Session) Value(key string) (interface{}, error) {
-	v, ok := s.data[key]
-	if !ok {
-		return nil, ErrKeyNotFound
-	}
-
-	return v, nil
+func (s *Session) Value(key string) interface{} {
+	return s.data[key]
 }
