aacc2b4566eb3bfdd70624a54aa9cd5e845f7a2b
chrislonng <chris@lonng.org>
2017-05-10 18:46:19 +0800

    add test and benchmark

--- serialize/json/json.go
+++ serialize/json/json.go
@@ -2,16 +2,16 @@ package json
 
 import "encoding/json"
 
-type Serialezer struct{}
+type Serializer struct{}
 
-func NewSerializer() *Serialezer {
-	return &Serialezer{}
+func NewSerializer() *Serializer {
+	return &Serializer{}
 }
 
-func (s *Serialezer) Serialize(v interface{}) ([]byte, error) {
+func (s *Serializer) Serialize(v interface{}) ([]byte, error) {
 	return json.Marshal(v)
 }
 
-func (s *Serialezer) Deserialize(data []byte, v interface{}) error {
+func (s *Serializer) Deserialize(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
